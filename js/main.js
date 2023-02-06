1fd97865b536317be92a4fadf3b057a9429c706d
Impuls08 <62505309+Impuls08@users.noreply.github.com>
2023-02-06 15:00:29 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+// Перевірка темної теми в localStorage
+if(localStorage.getItem('darkMode') === 'dark'){
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add('dark');
+} else if(localStorage.getItem('darkMode') === 'light'){
+  btnDarkMode.classList.remove("dark-mode-btn--active");
+  document.body.classList.remove('dark');
+}
+
+//перевірка темної теми на рівні системних настройок
+
+if(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add('dark');
+}
+
+//  якщо міняються системні настройки, міняємо тему
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', (event) => {
+  const newColorScheme = event.matches ? 'dark' : "light";
+
+  if (newColorScheme === 'dark'){
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add('dark');
+    localStorage.setItem("darkMode", 'dark');
+  } else {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove('dark');
+    localStorage.setItem("darkMode", 'light');
+  }
+})
+
+// Включеня нічного режиму
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle('dark');
+
+  if(isDark){
+    localStorage.setItem('darkMode', 'dark')
+  } else{
+    localStorage.setItem('darkMode', 'light');
+  }
+}
